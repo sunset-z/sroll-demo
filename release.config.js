39412d865d069723e613756d7114108e37d98d16
Emily <18312197836@163.com>
2024-06-26 16:29:21 +0800

    fix: npm prepare

--- release.config.js
+++ release.config.js
@@ -21,8 +21,14 @@ export default {
     ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ['@semantic-release/npm', { tarballDir: 'release' }],
-    '@semantic-release/github',
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'release',
+        prepare: 'node scripts/release.cjs',
+      },
+    ],
+    // '@semantic-release/github',
     '@semantic-release/git',
     [
       '@semantic-release/exec',
