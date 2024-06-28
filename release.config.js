b54f544931379397d8a6434490338d5fd42b284c
Emily <18312197836@163.com>
2024-06-28 15:46:58 +0800

    test: sequence

--- release.config.js
+++ release.config.js
@@ -20,15 +20,13 @@ export default {
         ],
       },
     ],
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    // [
-    //   '@semantic-release/npm',
-    //   {
-    //     // 更改 package.json 的 version
-    //     npmPublish: false, // 禁用 npm 发布
-    //   },
-    // ],
+    [
+      '@semantic-release/npm',
+      {
+        // 更改 package.json 的 version
+        npmPublish: false, // 禁用 npm 发布
+      },
+    ],
     [
       '@semantic-release/exec',
       {
@@ -39,14 +37,15 @@ export default {
         publishCmd: 'npm run release', // 打包、发布一体
       },
     ],
-    '@semantic-release/github',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     [
       '@semantic-release/git',
       {
         // assets: ['dist/**/*', 'package.json', 'CHANGELOG.md'], // 这里会跟根目录一同打包
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        prepare: 'echo "${nextRelease.version} not push"',
       },
     ],
+    '@semantic-release/github',
   ],
 };
