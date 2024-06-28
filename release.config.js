7239363446c9bae64e4349581fdeff473d2d6978
Emily <18312197836@163.com>
2024-06-28 17:03:56 +0800

    test: new branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,8 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
 export default {
-  branches: ['npm'],
+  branches: ['master'], // 基于
   repositoryUrl: 'https://github.com/sunset-z/sroll-demo.git',
   plugins: [
     [
@@ -22,45 +25,58 @@ export default {
     ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    // ------ 上面不变 ------
+    {
+      // 自定义打包命令，在 `prepare` 之前运行
+      path: '@semantic-release/exec',
+      cmd: 'npm run build',
+      // prepareCmd: 'npm run build',
+    },
+    {
+      // 自定义推送命令，将构建产物、package.json 和 CHANGELOG.md 推送到指定分支
+      path: '@semantic-release/exec',
+      cmd: `
+        git config --global user.name "semantic-release-bot"
+        git config --global user.email "semantic-release-bot@example.com"
+        git checkout -b release-branch
+        git add -f dist/ package.json CHANGELOG.md
+        git commit -m "chore(release): \${nextRelease.version} [skip ci]"
+        git push --force origin release-branch
+      `,
+      // successCmd: `
+      //   git config --global user.name "semantic-release-bot"
+      //   git config --global user.email "semantic-release-bot@example.com"
+      //   git checkout -b release-branch
+      //   git add -f dist/ package.json CHANGELOG.md
+      //   git commit -m "chore(release): \${nextRelease.version} [skip ci]"
+      //   git push --force origin release-branch
+      // `,
+    },
+
     [
       '@semantic-release/npm',
-      {
-        // 更改 package.json 的 version
-        npmPublish: false, // 禁用 npm 发布
-      },
+      // {
+      //   // 更改 package.json 的 version
+      //   npmPublish: false, // 禁用 npm 发布
+      // },
     ],
-    [
-      '@semantic-release/exec',
-      {
-        verifyReleaseCmd: 'echo "Verifying release version ${nextRelease.version}"',
-        // prepareCmd: 'yarn pack:release', // 自定义打包脚本
-        // publishCmd: 'npm publish', // 未指定具体 tgz 文件，将会再执行 npm pack
-        prepareCmd: 'npm run build:npm', // 下载依赖
-        publishCmd: 'npm run release', // 打包、发布一体
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['dist/**/*'], // 这里会跟根目录一同打包
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    // [
+    //   '@semantic-release/exec',
+    //   {
+    //     verifyReleaseCmd: 'echo "Verifying release version ${nextRelease.version}"',
+    //     // prepareCmd: 'yarn pack:release', // 自定义打包脚本
+    //     // publishCmd: 'npm publish', // 未指定具体 tgz 文件，将会再执行 npm pack
+    //     prepareCmd: 'npm run build:npm', // 下载依赖
+    //     publishCmd: 'npm run release', // 打包、发布一体
+    //   },
+    // ],
+    // [
+    // '@semantic-release/git',
+    // {
+    //   assets: ['dist/**/*', 'package.json', 'CHANGELOG.md'], // 这里会跟根目录一同打包 ????
+    //   message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    // },
+    // ],
     '@semantic-release/github',
   ],
-  verifyConditions: ['@semantic-release/changelog', '@semantic-release/git', '@semantic-release/github'],
-  prepare: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
-      },
-    ],
-  ],
-  publish: ['@semantic-release/npm', '@semantic-release/github'],
-  success: ['@semantic-release/github'],
-  fail: ['@semantic-release/github'],
 };
