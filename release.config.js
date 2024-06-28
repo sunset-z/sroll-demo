cb2f4c190c80497598f199c536c9658109d99c62
Emily <18312197836@163.com>
2024-06-28 17:32:49 +0800

    test: push npm

--- release.config.js
+++ release.config.js
@@ -54,6 +54,14 @@ export default {
         // publishCmd: 'npm publish', // 未指定具体 tgz 文件，将会再执行 npm pack
         prepareCmd: 'npm run build:npm', // 下载依赖
         publishCmd: 'npm run release', // 打包、发布一体
+        successCmd: `
+          git config --global user.name "semantic-release-bot"
+          git config --global user.email "semantic-release-bot@martynus.net"
+          git checkout -b npm
+          git add -f dist/ package.json CHANGELOG.md .gitignore
+          git commit -m "chore(release): \${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+          git push --force origin npm
+        `,
       },
     ],
     // [
